37cc9513777a94ca985d0c48be8ce97a5a63a671
zabaidulbd <zislam5622@gmail.com>
2023-05-02 19:18:37 +0600

    add cors

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const chef = require('./data/chef.json');
+
 app.get('/', (req, res) => {
     res.send('Chef Details Coming Soon')
 });
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chef.find(singleChef => singleChef.id === id);
+    res.send(selectedChef);
+
+})
 
 app.listen(port, () => {
     console.log(`use chef api : ${port}`)
